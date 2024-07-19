3b3617c3d727853d055d001529805687086d8ff9
AhnYeonghoo <dksdudgn00@naver.com>
2024-07-19 16:28:16 +0900

    d

---
+++ gabarge/While.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class While
+    {
+        static void MainWhile()
+        {
+            int i = 10;
+            while (i > 0)
+            {
+                Console.WriteLine($"i : {i--}");
+            }
+        }
+    }
+}
